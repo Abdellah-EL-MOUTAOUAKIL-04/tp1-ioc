21e63610b71673931ba0f961a7ec3c427d4fcf56
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-02-24 09:52:49 +0000

    feat: L'implémentation de l'interface IMetier en utilisant le couplage faible

---
+++ src/main/java/metier/IMetierImpl.java
@@ -0,0 +1,19 @@
+package metier;
+
+import dao.IDao;
+
+public class IMetierImpl implements IMetier {
+    //Couplage faible
+    private IDao dao;
+    @Override
+    public double calcul() {
+        double t=dao.getData();
+        double res=t*23;
+        return res;
+    }
+
+    //pour injecter dans la variable dao un objet d'une classe qui implemente l'interface IDao
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
